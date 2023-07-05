de7b216ddce12e3d1346c3d9aefbe43a41129294
Johannes <johannes>
2023-07-05 17:36:43 +0200

    added a camera class for full interaction with cam

--- main.py
+++ main.py
@@ -12,18 +12,23 @@ from pygame import Surface
 pygame.init()
 pygame.font.init()
 
-MAX_ROTATIONS = 32
+MAX_ROTATIONS = 100
 DEBUG_FONT_SIZE = 14
 DEBUG_MARGIN = 2
 DEBUG_ROTATION_SPEED = 10
 ASSET_PATH = 'assets/'
 
-screen = pygame.display.set_mode((720, 720), 0, 32)
-display = pygame.Surface((500,500))
+screen = pygame.display.set_mode((1920, 1080), 0, 32)
+display = pygame.Surface((720,720))
 
 font_renderer = pygame.font.SysFont("Arial", DEBUG_FONT_SIZE)
 
 clock = pygame.time.Clock()
+
+class Camera:
+    def __init__(self, offset: list[int, int], rotation: int) -> None:
+        self.offset = offset
+        self.rotation = rotation
 
 def split_stylesheet_into_chunks(stylesheet_path: str) -> list[Surface]:
     stylesheet = pygame.image.load(stylesheet_path)
@@ -66,20 +71,20 @@ def render_stack(surf: Surface, images: list[Surface], pos: tuple[int, int], rot
 def get_rotation_index(degrees: int) -> int:
     return int(-degrees * MAX_ROTATIONS // 360) % MAX_ROTATIONS
 
-def render_from_matrix(surf: Surface, matrix: dict[int, Surface], pos: tuple[int, int], rotation: int, camera_rotation: int) -> None:
-    rotation_offset = get_rotation_index(camera_rotation)
+def render_from_matrix(surf: Surface, matrix: dict[int, Surface], pos: tuple[int, int], rotation: int, camera: Camera) -> None:
+    rotation_offset = get_rotation_index(camera.rotation)
     final_rotation = (rotation + rotation_offset) % MAX_ROTATIONS
 
     img = matrix[final_rotation]
 
     x, y = pos
-    camera_rotation_radians = math.radians(camera_rotation)
+    camera_rotation_radians = math.radians(camera.rotation)
 
     screen_center_x = surf.get_width() // 2
     screen_center_y = surf.get_height() // 2
 
-    offset_x = (x - screen_center_x) * math.cos(camera_rotation_radians) - (y - screen_center_y) * math.sin(camera_rotation_radians)
-    offset_y = (x - screen_center_x) * math.sin(camera_rotation_radians) + (y - screen_center_y) * math.cos(camera_rotation_radians)
+    offset_x = (x - screen_center_x) * math.cos(camera_rotation_radians) - (y - screen_center_y) * math.sin(camera_rotation_radians) + camera.offset[0]
+    offset_y = (x - screen_center_x) * math.sin(camera_rotation_radians) + (y - screen_center_y) * math.cos(camera_rotation_radians) + camera.offset[1]
 
     render_x = int(screen_center_x + offset_x - img.get_width() // 2)
     render_y = int(screen_center_y + offset_y - img.get_height() // 2)
@@ -93,7 +98,8 @@ def make_asset_map(asset_path: str = ASSET_PATH) -> dict[str, dict[int, Surface]
 
 asset_map = make_asset_map()
 frame = 0
-forced_rotation = 0
+
+camera = Camera([0,0], 0)
 
 while True:
     current_rotation = frame // DEBUG_ROTATION_SPEED % MAX_ROTATIONS
@@ -103,9 +109,9 @@ while True:
     i = 0
     for key, asset in asset_map.items():
         i += 1
-        render_from_matrix(display, asset, (75 * i, display.get_height() // 2), 0, forced_rotation)
+        render_from_matrix(display, asset, (75 * i, display.get_height() // 2), 0, camera)
 
-    rotation_debug = font_renderer.render(f'rotation: {get_rotation_index(forced_rotation)}', False, (255,255,255))
+    rotation_debug = font_renderer.render(f'rotation: {get_rotation_index(camera.rotation)}', False, (255,255,255))
     asset_debug = font_renderer.render(f'assets loaded: {len(asset_map)}', False, (255,255,255))
     display.blit(rotation_debug, (0,0))
     display.blit(asset_debug, (0, DEBUG_FONT_SIZE + DEBUG_MARGIN))
@@ -114,12 +120,28 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
         if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+
             if event.key == pygame.K_q:
-                forced_rotation -= 10
+                camera.rotation -= 10
 
             if event.key == pygame.K_e:
-                forced_rotation += 10
+                camera.rotation += 10
+
+            if event.key == pygame.K_a:
+                camera.offset[0] -= 10
+
+            if event.key == pygame.K_d:
+                camera.offset[0] += 10
+
+            if event.key == pygame.K_w:
+                camera.offset[1] -= 10
+
+            if event.key == pygame.K_s:
+                camera.offset[1] += 10
 
     screen.blit(pygame.transform.scale(display, screen.get_size()), (0,0))
     pygame.display.update()
